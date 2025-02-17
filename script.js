2d56b0ec54410397bfd2bdb17f188679dc8c9573
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-17 16:46:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-//complete this code
-class Rectangle {}
+//complete this cobde
+class Rectangle {
+	constructor(width,height){
+		this.width=width;
+		this.height=height;
+	}
+	get width(this.width){
+		return this._width
+	}
+	get height (this.height){
+		return this._height 
+	}
+	get Area(){
+		return 2*(this._width*this._height )
+	}
+	
+}
 
-class Square extends Animal {}
+class Square extends Rectangle {
+	constructor (width){
+		super(width,width)
+	}
+
+	get Permiter(width,width){
+		return 2*(this._width +this._width)
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
