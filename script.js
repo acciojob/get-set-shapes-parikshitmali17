b7c2999fcd60d9a196a3f34a521158f1d1b4c9bd
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-17 17:25:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,80 @@
-//complete this cobde
-class Rectangle {
-	constructor(width,height){
-		if(width<=0 || height<=0){
-			throw new error("width and height must be positive integer")
-		}else{
-			this.width=width;
-		this.height=height;
-		}
+// //complete this cobde
+// class Rectangle {
+// 	constructor(width,height){
+// 		if(width<=0 || height<=0){
+// 			throw new error("width and height must be positive integer")
+// 		}else{
+// 			this.width=width;
+// 		this.height=height;
+// 		}
 		
-	}
-	get width(){
-		return this._width
-	}
-	get height (){
-		return this._height 
-	}
-	getArea(){
-		return (this._width*this._height )
-	}
+// 	}
+// 	get width(){
+// 		return this._width
+// 	}
+// 	get height (){
+// 		return this._height 
+// 	}
+// 	getArea(){
+// 		return (this._width*this._height )
+// 	}
 	
+// }
+
+// class Square extends Rectangle {
+// 	constructor (side){
+// 		if(side<=0){
+// 			throw error("side must be positive integer")
+// 		}
+// 		super(side,side)
+// 	}
+
+// 	getPermiter(){
+// 		return 4*(this._side)
+// 	}
+// }
+
+// // Do not change the code below this line
+// window.Rectangle = Rectangle;
+// window.Square = Square;
+
+
+class Rectangle {
+    constructor(width, height) {
+        if (width <= 0 || height <= 0) {
+            throw new Error("Width and height must be positive integers.");
+        }
+        this._width = width;
+        this._height = height;
+    }
+
+    get width() {
+        return this._width;
+    }
+
+    get height() {
+        return this._height;
+    }
+
+    getArea() {
+        return this._width * this._height;
+    }
 }
 
 class Square extends Rectangle {
-	constructor (side){
-		if(side<=0){
-			throw error("side must be positive integer")
-		}
-		super(side,side)
-	}
+    constructor(side) {
+        if (side <= 0) {
+            throw new Error("Side must be a positive integer.");
+        }
+        super(side, side);
+    }
 
-	getPermiter(){
-		return 4*(this._side)
-	}
+    getPerimeter() {
+        return 4 * this._width;
+    }
 }
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
+
